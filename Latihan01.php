dc9e1ba2ef9af39425c2911233244868bcba6d50
Mahimaa18 <127836767+Mahimaa18@users.noreply.github.com>
2023-03-25 08:29:13 +0700

    Pertemuan 2

---
+++ Latihan01.php
@@ -0,0 +1,19 @@
+<?php
+    $judulpage = "Latihan 01 php";
+    $konten = '<h3> Latihan 01 php </h3> <p> Latihan Praktikum Pemrograman web bentuk dasar php </p>'
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $judulpage; ?> </title>
+</head>
+<body>
+    
+    <?php
+        echo $konten;
+    ?>
+
+</body>
+</html>
---
+++ Latihan02.php
@@ -0,0 +1,17 @@
+<?php
+    $angka = 3;
+
+    echo "Nilai Angka: <strong>". $angka."</strong>";
+
+    if($angka > 4){
+        //kondisitrue
+        echo "<br>Nilai amgka lebih dari 4";
+
+    }else{
+        //kondisi false
+        echo "<br>Nilai angka kurang dari sama dengan 4";
+
+    }
+
+    // ! = not
+    
